f6bc668f1fcbec66a1bef2ff6929eac2b0827812
Montasir hasan <montasirhasan65@gmail.com>
2025-03-09 18:53:38 +0600

    Update ads.js

--- dog/material/js/ads.js
+++ dog/material/js/ads.js
@@ -1,4 +1,4 @@
-const AdController = window.Adsgram.init({ blockId: "7986362957" });
+const AdController = window.Adsgram.init({ blockId: "int-8810" });
 AdController.show().then((result) => {
     // user watch ad till the end or close it in interstitial format
     // your code to reward user for rewarded format
